31a3a7d706ba704d0705ea8582fcf379cfceef32
George CAO <matrix3456@gmail.com>
2022-01-05 22:02:58 +0800

    rename to mojo

--- json2bean-maven-plugin/src/main/java/org/reploop/plugin/maven/Json2BeanMojo.java
+++ json2bean-maven-plugin/src/main/java/org/reploop/plugin/maven/Json2BeanMojo.java
@@ -5,11 +5,22 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.reploop.translator.json.bean.Json2Bean;
+import org.reploop.translator.json.bean.Json2BeanApp;
+import org.reploop.translator.json.bean.JsonMessageContext;
+
+import java.io.IOException;
 
 @Mojo(name = "json2bean")
 public class Json2BeanMojo extends AbstractMojo {
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+        Json2BeanApp app = new Json2BeanApp();
         Json2Bean json2Bean = new Json2Bean();
+        JsonMessageContext ctx = new JsonMessageContext();
+        try {
+            json2Bean.execute("ctx", ctx);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
