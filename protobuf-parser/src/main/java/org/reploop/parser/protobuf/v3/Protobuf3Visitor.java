a61bb661c367d06008e7d78b4a11ce8ee0a51e78
George CAO <matrix3456@gmail.com>
2023-11-05 09:45:13 +0800

    Regenerate Protobuf3

--- protobuf-parser/src/main/java/org/reploop/parser/protobuf/v3/Protobuf3Visitor.java
+++ protobuf-parser/src/main/java/org/reploop/parser/protobuf/v3/Protobuf3Visitor.java
@@ -1,4 +1,4 @@
-// Generated from java-escape by ANTLR 4.11.1
+// Generated from org/reploop/parser/protobuf/v3/Protobuf3.g4 by ANTLR 4.13.1
 package org.reploop.parser.protobuf.v3;
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 
